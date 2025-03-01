2e1427064eb1331f7a9cde9d229c92f09477cd55
paulmuir1981 <paulmuir1981@gmail.com>
2025-03-01 14:34:11 +0000

    Add tests

--- Skittles.Tests/WebTests.cs
+++ Skittles.Tests/WebTests.cs
@@ -1,27 +1,69 @@
+using Aspire.Hosting;
+
 namespace Skittles.Tests;
 
-public class WebTests
+public class WebTests : IDisposable
 {
-    [Test]
-    public async Task GetWebResourceRootReturnsOkStatusCode()
+    private DistributedApplication _app;
+    private HttpClient _httpClient;
+
+    [OneTimeSetUp]
+    public async Task OneTimeSetUp()
     {
-        // Arrange
         var appHost = await DistributedApplicationTestingBuilder.CreateAsync<Projects.Skittles_AppHost>();
         appHost.Services.ConfigureHttpClientDefaults(clientBuilder =>
         {
             clientBuilder.AddStandardResilienceHandler();
         });
 
-        await using var app = await appHost.BuildAsync();
-        var resourceNotificationService = app.Services.GetRequiredService<ResourceNotificationService>();
-        await app.StartAsync();
+        _app = await appHost.BuildAsync();
+        var resourceNotificationService = _app.Services.GetRequiredService<ResourceNotificationService>();
+        await _app.StartAsync();
+        _httpClient = _app.CreateHttpClient("webfrontend");
+        await resourceNotificationService.WaitForResourceAsync("webfrontend", KnownResourceStates.Running)
+            .WaitAsync(TimeSpan.FromSeconds(30));
+    }
 
-        // Act
-        var httpClient = app.CreateHttpClient("webfrontend");
-        await resourceNotificationService.WaitForResourceAsync("webfrontend", KnownResourceStates.Running).WaitAsync(TimeSpan.FromSeconds(30));
-        var response = await httpClient.GetAsync("/");
+    [OneTimeTearDown]
+    public void OneTimeTearDown()
+    {
+        Dispose();
+    }
+
+    [Test]
+    public async Task GetWebResourceRootReturnsOkStatusCode()
+    {
+        //// Act
+        var response = await _httpClient.GetAsync("/");
 
         // Assert
         Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+    }
+
+    [Test]
+    public async Task GetWebResourcePlayersReturnsOkStatusCode()
+    {
+        // Act
+        var response = await _httpClient.GetAsync("/players");
+
+        // Assert
+        Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+    }
+
+    [Test]
+    public async Task GetWebResourceRandomUriReturnsNotFoundStatusCode()
+    {
+        // Act
+        var response = await _httpClient.GetAsync($"/{Guid.NewGuid}");
+
+        // Assert
+        Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.NotFound));
+    }
+
+    public void Dispose()
+    {
+        _httpClient?.Dispose();
+        _app?.Dispose();
+        GC.SuppressFinalize(this);
     }
 }
