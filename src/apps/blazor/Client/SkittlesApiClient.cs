e7602e0700463d9540c653f758d6189d68d891fc
paulmuir1981 <paulmuir1981@gmail.com>
2025-03-02 02:13:09 +0000

    still not broken...

---
+++ src/apps/blazor/Client/SkittlesApiClient.cs
@@ -0,0 +1,23 @@
+﻿using System.Net.Http.Json;
+
+namespace Client;
+
+public class SkittlesApiClient(HttpClient httpClient)
+{
+    public async Task<Player[]> GetPlayersAsync(CancellationToken cancellationToken = default)
+    {
+        List<Player>? players = null;
+        //todo i don't think this is needed. example app had a maximum number and would quit after that. just deserialise the array/list/whatever
+        await foreach (var player in httpClient.GetFromJsonAsAsyncEnumerable<Player>("/players", cancellationToken))
+        {
+            if (player is not null)
+            {
+                players ??= [];
+                players.Add(player);
+            }
+        }
+        return players?.ToArray() ?? [];
+    }
+}
+
+public record Player(Guid Id, string Name);
