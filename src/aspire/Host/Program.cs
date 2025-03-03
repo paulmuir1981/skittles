64757546b10a2f8fc64870258245ce02a85c1b21
paulmuir1981 <paulmuir1981@gmail.com>
2025-03-03 16:10:06 +0000

    remove skittles web proj

--- src/aspire/Host/Program.cs
+++ src/aspire/Host/Program.cs
@@ -1,20 +1,7 @@
 var builder = DistributedApplication.CreateBuilder(args);
-
-//var cache = builder.AddRedis("cache");
-
 var webapi = builder.AddProject<Projects.Skittles_ApiService>("webapi");
-
-//builder.AddProject<Projects.Skittles_Web>("blazor")
-//    .WithExternalHttpEndpoints()
-//    .WithReference(cache)
-//    .WaitFor(cache)
-//    .WithReference(webapi)
-//    .WaitFor(webapi);
-
-builder.AddProject<Projects.Client>("client")
+builder.AddProject<Projects.Client>("blazor")
     .WithExternalHttpEndpoints()
-//    .WithReference(cache)
-//    .WaitFor(cache)
     .WithReference(webapi)
     .WaitFor(webapi);
 
